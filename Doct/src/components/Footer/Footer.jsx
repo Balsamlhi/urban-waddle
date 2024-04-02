63feaaff494cc060a33d518e0cfd5857949b8374
Balsam Lhi <balsamlhi50@gmail.com>
2024-04-02 23:24:42 +0100

    foooteer changes

--- Doct/src/components/Footer/Footer.jsx
+++ Doct/src/components/Footer/Footer.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
-import styles from '../Footer/Footer.module.css';
-//import logo from '../../assets/logo.png';
-
+import styles from '../footer/Footer.module.css';
+import logo from '../../assets/logo.png';
+import {
+  TiSocialFacebook,
+  TiSocialLinkedin,
+  TiSocialTwitter,
+  TiSocialInstagram,
+} from 'react-icons/ti'
 function Footer() {
   return (
     <footer className={styles.siteFooter}>
       <div className={styles.container}>
-        <div className={styles.row}>
-          <div className={`${styles.col} ${styles.colSm12} ${styles.colMd6}`}>
-            <img className={styles.logo}  alt="logo" />
+        <div className={styles.row1}>
+          <div className={styles.part1}>
+            <img className={styles.logo} src={logo} alt="logo" />
             <p className={styles.textJustify}>PRENEZ VOS RENDEZ-VOUS MÉDICAUX EN LIGNE FACILEMENT, RAPIDEMENT ET GRATUITEMENT</p>
             <h3><i className={styles.faMapMarkerAlt}></i> Tunis</h3>
           </div>
 
-          <div className={`${styles.col} ${styles.col6} ${styles.colMd3}`}>
+        <div className={styles.row2}>
             <h6>Service client</h6>
             <ul className={styles.footerLinks}>
               <li><a href="#">accueil</a></li>
@@ -22,7 +27,7 @@ function Footer() {
             </ul>
           </div>
 
-          <div className={`${styles.col} ${styles.col6} ${styles.colMd3}`}>
+        <div className={styles.row3}>
             <h6>CONTACTEZ NOUS</h6>
             <ul className={styles.footerLinks}>
               <br />
@@ -33,19 +38,27 @@ function Footer() {
         <hr className={styles.small} />
       </div>
       <div className={styles.container}>
-        <div className={styles.row}>
-          <div className={`${styles.col} ${styles.colMd8} ${styles.colSm6} ${styles.col12}`}>
+        <div className={styles.row4}>
+          <div className={styles.part}>
             <p className={styles.copyrightText}>Copyright © 2024 All Rights Reserved by
               <a href="#">Code4Education</a>.
             </p>
           </div>
 
-          <div className={`${styles.col} ${styles.colMd4} ${styles.colSm6} ${styles.col12}`}>
+          <div className={styles.lesicones}>
             <ul className={styles.socialIcons}>
-              <li><a className={styles.facebook} href="#"><i className={styles.fabFacebookF}></i></a></li>
-              <li><a className={styles.twitter} href="#"><i className={styles.fabTwitter}></i></a></li>
-              <li><a className={styles.dribbble} href="#"><i className={styles.fabDribbble}></i></a></li>
-              <li><a className={styles.linkedin} href="#"><i className={styles.fabLinkedinIn}></i></a></li>
+              <li><a className={styles.facebook} href="#">
+                <TiSocialFacebook />
+                </a></li>
+              <li><a className={styles.twitter} href="#">
+                <TiSocialTwitter />
+                </a></li>
+              <li><a className={styles.instagram} href="#">
+                <TiSocialInstagram />
+                </a></li>
+              <li><a className={styles.linkedin} href="#">
+                <TiSocialLinkedin />
+                </a></li>
             </ul>
           </div>
         </div>
@@ -55,3 +68,4 @@ function Footer() {
 }
 
 export default Footer;
+
